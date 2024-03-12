e0ca323614a9e373da873d0cfa9f354b8cc2e9dd
Jørgen Eriksson Midtbø <jorgenem@gmail.com>
2024-03-12 15:22:09 +0100

    - Added Anja - Added a one-line script to build the site locally for testing

---
+++ build_and_run_locally.sh
@@ -0,0 +1,2 @@
+# Command to launch a locally hosted version of the site with the latest changes,f or testing. Taken from here: https://docs.github.com/en/pages/setting-up-a-github-pages-site-with-jekyll/testing-your-github-pages-site-locally-with-jekyll
+bundle exec jekyll serve
